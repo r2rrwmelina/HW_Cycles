5cfc1034d290f479bfcdcc27f8ffc4d4e4a1c4dc
MG <gmelina69@gmail.com>
2026-02-25 17:52:30 +0400

    Task 1 is solved

---
+++ src/main/java/ru/netology/javaqa/HW_Cycles/services/VacationCounterService.java
@@ -0,0 +1,17 @@
+package ru.netology.javaqa.HW_Cycles.services;
+public class VacationCounterService {
+
+    public int calculate(int income, int expenses, int restThreshold) { // доход, траты, кол-во накоплений для отпуска
+        int count = 0;
+        int threshold = 0;
+        for (int month = 0; month < 12; month++) {
+            if (threshold >= restThreshold) { // отдыхаем
+                count++;
+                threshold = (threshold / 3) - expenses;
+            } else {
+                threshold = threshold + income - expenses; // работаем
+            }
+        }
+        return count; // количество отпускных месяцев
+    }
+}
---
+++ src/test/java/ru/netology/javaqa/HW_Cycles/services/VacationCounterTest.java
@@ -0,0 +1,26 @@
+package ru.netology.javaqa.HW_Cycles.services;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class VacationCounterTest {
+
+    @Test
+    public void shouldCalcThreeRestMonthsFromTask() { // пример из задания
+        VacationCounterService service = new VacationCounterService();
+
+        int expected = 3;
+        int actual = service.calculate(10_000, 3000, 20_000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalcTwoMonthsFromTask() {
+        VacationCounterService service = new VacationCounterService();
+
+        int expected = 2;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
